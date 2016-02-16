7454bf6bb482ae5c9b913d3abe65fcabdab76441
TAKEI Yuya <takei.yuya+github@gmail.com>
2016-02-17 00:04:11 +0900

    getopt

--- README.md
+++ README.md
@@ -1,6 +1,69 @@
 - TODO:
     - logging
     - error handing
-    - params by getopt
     - README
+    - systemd recipe
+    - check connected user
+    - security
 
+# かんたんなつかいかた (with ssh-agent)
+
+1. socket_multiplexer をバックグラウンドで動かす
+
+    めんどい場合はscreenの中とかでok (TODO: そのうちsystemd向けのserviceファイルこしらえる)
+
+    ```bash
+    socket_multiplexer -c /tmp/socket_multiplexer_control -s /tmp/socket_multiplexer
+    ```
+
+1. .bashrcでSSH_AUTH_SOCKをフックするようにする
+
+    たとえばこんな感じ。
+
+    ```bash
+    # ssh-agent使ったセッション、かつ、socket_multiplexerが動いてるなら
+    if [ -n "${SSH_AUTH_SOCK}" \
+      -a -S /tmp/socket_multiplexer_control \
+      -a -S /tmp/socket_multiplexer ]; then
+
+      # このセッションのSSH_AUTH_SOCKをsocket_multiplexerに追加
+      nc -U /tmp/socket_multiplexer_control <<<"ADD ${SSH_AUTH_SOCK}"
+
+      # 念のためSSH_AUTH_SOCKのバックアップをとって
+      ORIGINAL_SSH_AUTH_SOCK="${SSH_AUTH_SOCK}"
+
+      # socket_multiplexerに繋ぎ変えてしまう。
+      SSH_AUTH_SOCK=/tmp/socket_multiplexer
+    fi
+    ```
+
+1. 普通にsshで繋いだり、screenをデタッチ/アタッチしたりする。
+
+    sshがagentを要求する際、socket_multiplexerを通じて生きているソケットに勝手に繋がるようになるので、
+    screenのデタッチ/アタッチなどで環境変数を気にする必要がない！ しあわせ！
+
+1. そのほか、nc(1)などでソケットにメッセージを送ることで、操作が多少できます。
+
+    ```console
+    $ # socket_multiplexerにソケットを追加する。
+    $ nc -U /tmp/socket_multiplexer_control <<<"ADD /tmp/ssh-YXWG8gnlm1/agent.6715"
+    ADDed /tmp/ssh-YXWG8gnlm1/agent.6715
+
+    $ # ソケット一覧を確かめる
+    $ nc -U /tmp/socket_multiplexer_control <<<"LIST"
+    /tmp/ssh-rqzUdGXohX/agent.2751
+    /tmp/ssh-NCBXOhOmAX/agent.14009
+    /tmp/ssh-FXjiQezLPa/agent.3413
+    /tmp/ssh-HgfZlyTnCv/agent.4955
+    /tmp/ssh-YXWG8gnlm1/agent.6715
+
+    $ # ソケットをメンバから取り除く
+    $ nc -U /tmp/socket_multiplexer_control <<<"DELETE /tmp/ssh-NCBXOhOmAX/agent.14009"
+    DELETEed /tmp/ssh-NCBXOhOmAX/agent.14009
+
+    $ # 全部登録だ！
+    $ \ls -1 /tmp/ssh-*/agent.* | sed 's/^/ADD /' | nc -U /tmp/socket_multiplexer_control
+
+    $ # 終了する
+    $ nc -U /tmp/socket_multiplexer_control <<<"QUIT"
+    ```
--- src/main.cc
+++ src/main.cc
@@ -1,19 +1,72 @@
 #include <unistd.h>
 #include <signal.h>
+#include <getopt.h>
+#include <iostream>
 
 #include "socket_multiplexer.h"
 #include "config.h"
 
+namespace {
+const std::string kDefaultControlSocketFile = "/tmp/socket_multiplexer_control";
+const std::string kDefaultMasterSocketFile = "/tmp/socket_multiplexer";
+}
+
 std::unique_ptr<SocketMultiplexer> socket_multiplexer;
 
+void Usage(std::ostream& out, int argc, char** argv) {
+  out
+    << "Usage: " << argv[0] << std::endl
+    << std::endl
+    << "  -c, --control=PATH    control socket path (default: " << kDefaultControlSocketFile << ")" << std::endl
+    << "  -s, --socket=PATH     interface socket path (default: " << kDefaultMasterSocketFile << ")" << std::endl
+    << std::endl
+    << "  -?, --help            display this help and exit" << std::endl
+    << "  -v, --version         output version information and exit" << std::endl
+  ;
+}
+
 int main(int argc, char** argv) {
-  const std::string control_socket_path = "/tmp/socket_multiplexer_control";
-  const std::string interface_socket = "/tmp/socket_multiplexer";
+  SocketMultiplexer::Config config = { kDefaultControlSocketFile, kDefaultMasterSocketFile };
 
-  unlink(control_socket_path.c_str());
-  unlink(interface_socket.c_str());
+  struct option long_options[] = {
+    { "control",  required_argument,  0,  'c' },
+    { "socket",   required_argument,  0,  's' },
+    { "help",     no_argument,        0,  '?' },
+    { "version",  no_argument,        0,  'v' },
+    { 0, 0, 0, 0 }
+  };
 
-  SocketMultiplexer::Config config = { control_socket_path, interface_socket };
+  while (true) {
+    int ch = getopt_long(argc, argv, "?vc:s:", long_options, NULL);
+    if (ch < 0) {
+      break;
+    }
+
+    switch (ch) {
+    case 'c':
+      config.control_socket_path = optarg;
+      break;
+
+    case 's':
+      config.master_socket_path = optarg;
+      break;
+
+    case '?':
+      Usage(std::cout, argc, argv);
+      exit(0);
+
+    case 'v':
+      std::cout << PACKAGE_STRING << std::endl;
+      exit(0);
+
+    default:
+      Usage(std::cerr, argc, argv);
+      exit(1);
+    }
+  }
+
+  unlink(config.control_socket_path.c_str());
+  unlink(config.master_socket_path.c_str());
 
   socket_multiplexer.reset(new SocketMultiplexer(config));
 
